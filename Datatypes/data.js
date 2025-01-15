fc543d42b7635d091622a97b945f62ee47c88db0
Saurav Doke <sauravdoke@gmail.com>
2025-01-15 15:45:57 +0530

    variables and datatypes

---
+++ Datatypes/data.js
@@ -0,0 +1,13 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)  We are using Nodejs not browser
+
+let name ="saurav"; //string
+let age=20; //integer
+let Isteenager=false;//boolean
+let state;//undefined
+let roll_no=null //empty standalone
+
+//object
+
+console.log(typeof roll_no)
---
+++ Lec 1 Variables and constants/new.js
@@ -0,0 +1,25 @@
+const accountId =14453;
+let accountEmail="sauravdoke2622@gmail.com"
+var accountPassword ="26220405"
+accountCity ="Jaipur"
+let accountState;
+
+// accountId = 2 //Not Allowed
+
+accountEmail ="shubhadaj2226@gmail.com"
+accountPassword="22260504"
+accountCity="Mumbai"
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+ */
+//for table
+console.table([accountEmail,accountPassword,accountCity, accountState]);
+
+
+
+
