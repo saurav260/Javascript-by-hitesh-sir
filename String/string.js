c43a0ecb60cda8985e0d542543afc6deebe78ce8
Saurav Doke <sauravdoke@gmail.com>
2025-01-20 22:16:58 +0530

    Strings

---
+++ String/string.js
@@ -0,0 +1,23 @@
+const name = "saurav ";
+const fortsCount = 12;
+
+// console.log(name + fortsCount +" till 20 january 2025")  //It is old format
+
+console.log(
+  `Hello My Name is ${name} and I Travelled around ${fortsCount} forts till 20 january 2025`
+);
+
+
+const fortName= new String('Jivdhan-Junnar');
+
+console.log(fortName[1]);
+console.log(fortName.length);
+console.log(fortName.toUpperCase());
+console.log(fortName.toLowerCase());
+console.log(fortName.charAt(6))
+console.log(fortName.indexOf('a'))
+console.log(fortName.__proto__);
+console.log(fortName[1]);
+
+const newString=fortName.substring(0, 5)
+console.log(newString)
