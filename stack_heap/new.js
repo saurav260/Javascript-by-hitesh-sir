99f7f0fbc40b814dbf57c89f243255b0f25a63ee
Saurav Doke <sauravdoke@gmail.com>
2025-01-20 21:52:07 +0530

    stack and heap

---
+++ stack_heap/new.js
@@ -0,0 +1,23 @@
+//Stack=primitive ,Heap=Non premitive
+
+let myBestFriend= "vedant";
+
+let myanotherBestFriend=myBestFriend;
+myanotherBestFriend="Onkar";
+
+console.log(myBestFriend);
+console.log(myanotherBestFriend)
+
+
+let userOne ={
+
+    email:"saurav@travel.com",
+    upi:"7856129685@ybl"
+
+}
+
+let userTwo=userOne;
+
+userTwo.email= "ssd@travel.com "
+console.log(userOne.email)
+console.log(userTwo.email)
