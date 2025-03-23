5769e1b4d4d4bff0feb201ad3747fb0dbf490236
Saurav Doke <sauravdoke@gmail.com>
2025-03-23 11:59:30 +0530

    Switch case

---
+++ controlflow/switch.js
@@ -0,0 +1,24 @@
+// switch(key){
+//     case value:
+//         break;
+
+//         default:
+//             break;
+// }
+
+const month =3;
+switch (month) {
+    case 1:
+        console.log("january");
+         break;
+    case 2:
+        console.log("feb");
+         break;
+    case 3:
+        console.log("march");
+         break;
+
+    default:
+        console.log("Enter valid number between 1-3")
+        break;
+}
