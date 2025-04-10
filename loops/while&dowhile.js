11c51f3fe0c6462c735a77b7bca92d7cc9709887
Saurav Doke <sauravdoke@gmail.com>
2025-04-10 22:45:29 +0530

    while and do while loop

---
+++ loops/while&dowhile.js
@@ -0,0 +1,30 @@
+//while loop
+
+let index =0
+while (index<=10) {
+    console.log(`Value of index is : ${index}`);
+    index = index +2;
+    
+}
+
+//by array
+let myArray=["saurav","om","vedant","swanand"]
+let arr=0
+while (arr<myArray.length) {
+   console.log(`value of my Array is : ${myArray[arr]}`)
+    arr = arr+1;
+}
+
+//do while
+
+let score =1
+do {
+    console.log(` Score is :${score}`)
+    score++;
+} while (score<10);
+
+let run =11;
+do {
+    console.log(` Score is :${run}`)
+    run++;
+} while (run<10);
